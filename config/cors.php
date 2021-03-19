9cfefbd19171a78d3a6dc31a76572c9093597105
Adonai Nangui <adonainangui03@gmail.com>
2021-03-19 02:44:20 +0000

    Configure fortify and sanctum

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,17 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        '/login',
+        '/logout',
+        '/api/register',
+        'user/password',
+        'forgot-password',
+        'reset-password',
+        'user/profile-information',
+    ],
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +39,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware(['guest']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
